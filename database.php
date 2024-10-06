eee7e54f7f602199598a23104ebf22792dbb482e
Xy-Home0 <90628219+Xy-Home0@users.noreply.github.com>
2024-10-06 01:03:12 -0700

    Update database.php

--- database.php
+++ database.php
@@ -1,11 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root"; // adjust if needed
-$password = "";     // adjust if needed
+$username = "root"; 
+$password = "";     
 $dbname = "task_manager";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, 3307);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
