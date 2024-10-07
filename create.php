1d73459c04a3563e7455399869b0f504a4e858f5
King Xyro Casa <casaking44@gmail.com>
2024-10-07 05:08:20 -0700

    updated create.php from costales

--- create.php
+++ create.php
@@ -4,9 +4,10 @@ include 'functions.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $task_name = $_POST['task_name'];
     $task_description = $_POST['task_description'];
+    $due_date = $_POST['due_date'];
 
-    if (createTask($task_name, $task_description)) {
-        echo "New task created successfully";
+    if (createTask($task_name, $task_description, $due_date)) {
+        echo "Task created successfully";
     } else {
         echo "Error creating task";
     }
