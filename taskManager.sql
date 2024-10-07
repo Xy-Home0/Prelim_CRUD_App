182be62b8749df69cff3eca19c08f12649edebae
King Xyro Casa <casaking44@gmail.com>
2024-10-07 07:19:04 -0700

    updated taskManager.sql

--- taskManager.sql
+++ taskManager.sql
@@ -7,3 +7,11 @@ CREATE TABLE tasks (
     task_description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE tasks ADD due_date DATE;
